271ba4a664d92bcb536e4102f1836bb9bdf66850
Shamit Bhatia <shamit.bhatia2@gmail.com>
2026-08-27 19:54:36 -0700

    Repair authenticated component test harness (#25)

--- app/expenses/page.cy.tsx
+++ app/expenses/page.cy.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import Page from './page'
+import {AuthContext} from '@/app/context'
+
+const signedOutAuth = {
+  user: null,
+  loading: true,
+  isGuest: false,
+  continueAsGuest: () => undefined,
+  signOut: async () => undefined,
+}
 
 describe('<page />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<Page />)
+    cy.mount(<AuthContext.Provider value={signedOutAuth}><Page /></AuthContext.Provider>)
   })
 })
--- components/AddNewExpense.cy.tsx
+++ components/AddNewExpense.cy.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import AddNewExpense from './AddNewExpense'
+import {AuthContext} from '@/app/context'
+
+const signedOutAuth = {
+  user: null,
+  loading: false,
+  isGuest: false,
+  continueAsGuest: () => undefined,
+  signOut: async () => undefined,
+}
 
 describe('<AddNewExpense />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<AddNewExpense />)
+    cy.mount(<AuthContext.Provider value={signedOutAuth}><AddNewExpense /></AuthContext.Provider>)
   })
 })
